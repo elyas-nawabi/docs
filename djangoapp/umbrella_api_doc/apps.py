fba25d44976c7d0e06078bb303d2da2c7ea99202
Elyas Nawabi <elyas.nawabi19@gmail.com>
2024-01-28 20:23:45 +0430

    umbrella documentation restructured

--- djangoapp/umbrella_api_doc/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UmbrellaApiDocConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'umbrella_api_doc'
--- djangoapp/umbrella_api_doc/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def umbrella_api_doc(request):
-     return render(request, 'index.html')
